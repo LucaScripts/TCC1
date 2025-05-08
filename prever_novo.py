6f9ae8d051d830553ca94c9b5565e5f47a99582d
Lucas Dias <2019219060@ifba.edu.br>
2025-05-08 18:34:35 -0300

    feat: adicionar script de previsão para novos alunos com modelo salvo

---
+++ prever_novo.py
@@ -0,0 +1,23 @@
+﻿import pandas as pd
+import joblib
+
+# 1. Carregar o modelo treinado
+modelo = joblib.load('modelo_random_forest.pkl')
+
+# 2. Criar um novo exemplo de aluno (com as mesmas colunas usadas no treino!)
+novo_aluno = pd.DataFrame([{
+    'Módulo atual': 0,
+    'Faltas Consecutivas': 5,
+    'Idade': 25,
+    'Sexo': 1,
+    'Pend. Acad.': 0,
+    'Possui Pendência Financeira': 0,
+    'Bolsista': 1,
+    'Antecipou Parcela': 0
+}])
+
+# 3. Fazer a previsão
+predicao = modelo.predict(novo_aluno)
+
+# 4. Mostrar o resultado
+print(f'Previsão da Situação (código): {predicao[0]}')
