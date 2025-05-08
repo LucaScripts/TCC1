77ea50209622e7222f386fef94a2876aa166d53e
Lucas Dias <2019219060@ifba.edu.br>
2025-05-08 19:24:59 -0300

    Refatore a estrutura do código para uso da nova planilha_final2.csv contendo todas as colunas e removendo no modelo

--- Random Forest/prever_novo.py
+++ Random Forest/prever_novo.py
@@ -7,12 +7,12 @@ modelo = joblib.load('modelo_random_forest.pkl')  # Carrega o modelo Random Fore
 # 2. Criar um novo exemplo de aluno (com as mesmas colunas usadas no treino!)
 novo_aluno = pd.DataFrame([{
     'Módulo atual': 2,
-    'Faltas Consecutivas': 0,
-    'Idade': 25,
-    'Sexo': 1,
+    'Faltas Consecutivas': 3,
+    'Idade': 16,
+    'Sexo (código)': 1, 
     'Pend. Acad.': 0,
-    'Possui Pendência Financeira': 2,
-    'Bolsista': 0,
+    'Possui Pendência Financeira': 0,
+    'Bolsista': 1,
     'Antecipou Parcela': 0
 }])
 
